876e7b5b7d89bee3b4c6ae48d84656fcedd0316e
Anton Karsseev <akarseev@nao.design>
2026-07-16 19:44:44 +0200

    some fix

--- subsys/usb/device_next/class/usbd_cdc_acm.c
+++ subsys/usb/device_next/class/usbd_cdc_acm.c
@@ -185,7 +185,7 @@ static int usbd_cdc_acm_init_wq(void)
 	k_work_queue_start(&cdc_acm_work_q, cdc_acm_stack,
 			   K_KERNEL_STACK_SIZEOF(cdc_acm_stack),
 			   CONFIG_USBD_CDC_ACM_WORKQUEUE_PRIORITY, NULL);
-	k_thread_name_set(&cdc_acm_work_q.thread_id, "cdc_acm_work_q");
+	k_thread_name_set(cdc_acm_work_q.thread_id, "cdc_acm_work_q");
 
 	return 0;
 }
